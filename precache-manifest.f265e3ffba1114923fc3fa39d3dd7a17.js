d343bf63fd6caec907cfd257a815d66eb827daad
Yan Te <y.te@semrush.com>
2019-01-21 22:26:01 +0300

    Updates

---
+++ precache-manifest.f265e3ffba1114923fc3fa39d3dd7a17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c45cc5c9cc978b217db",
+    "url": "/url-shorener-frontend/static/css/main.2f323ac9.chunk.css"
+  },
+  {
+    "revision": "4c45cc5c9cc978b217db",
+    "url": "/url-shorener-frontend/static/js/main.4c45cc5c.chunk.js"
+  },
+  {
+    "revision": "b577630992246ba1a15c",
+    "url": "/url-shorener-frontend/static/js/1.b5776309.chunk.js"
+  },
+  {
+    "revision": "34762ffe6d9642e9b1b5",
+    "url": "/url-shorener-frontend/static/js/runtime~main.34762ffe.js"
+  },
+  {
+    "revision": "e8323c22a589e41b896063e7dcc4543a",
+    "url": "/url-shorener-frontend/index.html"
+  }
+];
